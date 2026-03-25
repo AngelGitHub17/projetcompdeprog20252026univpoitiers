b73d2a603f12a954a86b4e382fdcb2f245e887bf
AngelGitHub17 <kariaclub52@gmail.com>
2026-03-25 15:04:53 +0100

    new upload part for the project - go to version 2

---
+++ Iteration2/Tetris/NoyauTetris/NoyauTetris.cs
@@ -0,0 +1,23 @@
+﻿namespace NoyauTetris;
+
+public enum Couleur
+{
+    Bleu,
+    Rose,
+    Violet,
+    Blanc,
+    Noir
+    
+}
+public class JeuTetris
+{
+    public int LargeurGrille;
+    public int HauteurGrille;
+
+    public JeuTetris()
+    {
+        LargeurGrille = 12;
+        HauteurGrille = 15;
+    }
+
+}
